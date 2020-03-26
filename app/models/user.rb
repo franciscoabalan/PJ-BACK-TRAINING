d61badd19b2a5291dcdc1cf1959e56ff32028d8b
blassoto <blas.soto@pjchile.cl>
2020-03-26 12:54:04 -0300

    Integration with devise-token-auth finished!

---
+++ app/models/user.rb
@@ -0,0 +1,11 @@
+class User < ActiveRecord::Base
+  extend Devise::Models
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+  include DeviseTokenAuth::Concerns::User
+
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+end
