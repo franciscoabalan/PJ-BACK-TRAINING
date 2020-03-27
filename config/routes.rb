d02e3bd7fc65b792bd1718402749ed5256dcf3e6
blassoto <blas.soto@pjchile.cl>
2020-03-27 16:40:56 -0300

    Index and Show Book implemented

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
   mount_devise_token_auth_for 'User', at: 'login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  namespace :api do
+    namespace :v1 do
+      resources :books, only: [:index, :show]
+    end
+  end
 end
