fdfe4d275c0b86aec22d353b798d38ebb179b56e
blassoto <blas.soto@pjchile.cl>
2020-03-27 17:01:03 -0300

    add email

--- db/schema.rb
+++ db/schema.rb
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20200326145838) do
     t.string "unconfirmed_email"
     t.string "first_name"
     t.string "last_name"
+    t.string "email"
     t.integer "sign_in_count", default: 0, null: false
     t.datetime "current_sign_in_at"
     t.datetime "last_sign_in_at"
