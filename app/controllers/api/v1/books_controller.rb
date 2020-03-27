93af0c9f2b28a7043ef68c7fe686c1815359cc94
blassoto <blas.soto@pjchile.cl>
2020-03-27 16:40:56 -0300

    Index and Show Book implemented

---
+++ app/controllers/api/v1/books_controller.rb
@@ -0,0 +1,28 @@
+module Api
+  module V1
+    class BooksController < ApplicationController
+      include Wor::Paginate
+
+      before_action :authenticate_user!
+      before_action :set_api_v2_book, only: [:show] # , :update, :destroy]
+
+      # GET /api/v2/books
+      def index
+        @api_v2_books = Book.all
+
+        render_paginated @api_v2_books
+      end
+
+      # GET /api/v2/books/1
+      def show
+        render_paginated @api_v2_book
+      end
+
+      private
+
+      def set_api_v2_book
+        @api_v2_book = Book.find(params[:id])
+      end
+    end
+  end
+end
