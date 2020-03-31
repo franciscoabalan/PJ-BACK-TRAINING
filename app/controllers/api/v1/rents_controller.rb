f7853b85b49866f4ba95391eab572e561209c007
blassoto <blas.soto@pjchile.cl>
2020-03-31 11:11:07 -0300

    fixed return code and exception

--- app/controllers/api/v1/rents_controller.rb
+++ app/controllers/api/v1/rents_controller.rb
@@ -1,18 +1,22 @@
 module Api
   module V1
-    class RentsController < ApplicationController
+    class RentsController < ApiController
       include Wor::Paginate
 
       # GET /api/v1/:user_id/rents
       def index
         render_paginated rents
+      rescue ActiveRecord::RecordNotFound => e
+        render json: { error: e.message }, status: :not_found
       end
 
       # POST /api/v1/rents
       def create
         rent = Rent.create!(rent_params)
-        render json: rent
-      rescue StandardError => e
+        render json: rent, status: :created
+      rescue ActionController::ParameterMissing => e
+        render json: { error: e.message }, status: :bad_request
+      rescue ActiveRecord::RecordInvalid => e
         render json: { error: e.message }, status: :unprocessable_entity
       end
 
